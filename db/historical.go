73546395b16773e8e3b671205f48a201731c6ec3
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-11 20:29:48 +0200

    refactor historical service for mongo

--- db/historical.go
+++ db/historical.go
@@ -4,53 +4,84 @@ import (
 	"context"
 	"time"
 
-	"github.com/vectorman1/analysis/analysis-api/model/db"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"github.com/Masterminds/squirrel"
+	"github.com/vectorman1/analysis/analysis-api/model/db/documents"
+
+	"github.com/vectorman1/analysis/analysis-api/common"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
 	"github.com/jackc/pgx"
 )
 
 type historicalRepository interface {
-	GetBySymbolUuid(ctx *context.Context, symbolUuid string, startDate time.Time, endDate time.Time) (*[]db.Historical, error)
+	GetBySymbolUuid(ctx *context.Context, symbolUuid string, startDate time.Time, endDate time.Time) (*[]documents.Historical, error)
 }
 
 type HistoricalRepository struct {
 	historicalRepository
-	db *pgx.ConnPool
+	pgdb    *pgx.ConnPool
+	mongodb *mongo.Database
 }
 
-func NewHistoricalRepository(db *pgx.ConnPool) *HistoricalRepository {
+func NewHistoricalRepository(db *pgx.ConnPool, mongodb *mongo.Database) *HistoricalRepository {
 	return &HistoricalRepository{
-		db: db,
+		pgdb:    db,
+		mongodb: mongodb,
 	}
 }
 
-func (r *HistoricalRepository) GetBySymbolUuid(ctx *context.Context, symbolUuid string, startDate time.Time, endDate time.Time) (*[]db.Historical, error) {
-	queryBuilder := squirrel.
-		Select("*").
-		From("analysis.historical").
-		Where(squirrel.Eq{"symbol_uuid": symbolUuid}).
-		Where(squirrel.LtOrEq{"for_date": endDate}).
-		Where(squirrel.GtOrEq{"for_date": startDate}).
-		PlaceholderFormat(squirrel.Dollar)
+func (r *HistoricalRepository) InsertMany(ctx context.Context, list []documents.Historical) (int, error) {
+	var e []interface{}
+	for _, v := range list {
+		e = append(e, v)
+	}
 
-	query, args, err := queryBuilder.ToSql()
+	res, err := r.mongodb.Collection(common.HISTORIES_COLLECTION).
+		InsertMany(ctx, e)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(res.InsertedIDs), nil
+}
+
+func (r *HistoricalRepository) GetBySymbolUuid(ctx context.Context, symbolUuid string, startDate time.Time, endDate time.Time) (*[]documents.Historical, error) {
+	opts := options.Find()
+	opts.SetSort(bson.D{{"timestamp", -1}})
+	filter := bson.M{
+		"symboluuid": symbolUuid,
+		"$and": []bson.M{
+			{
+				"timestamp": bson.M{
+					"$lt": endDate,
+				},
+			},
+			{
+				"timestamp": bson.M{
+					"$gt": startDate,
+				},
+			},
+		},
+	}
+
+	filterCursor, err := r.mongodb.Collection(common.HISTORIES_COLLECTION).
+		Find(ctx, filter, opts)
 	if err != nil {
 		return nil, err
 	}
+	defer filterCursor.Close(ctx)
 
-	rows, err := r.db.QueryEx(*ctx, query, &pgx.QueryExOptions{}, args)
-	if err != nil {
-		return nil, err
-	}
-
-	var result []db.Historical
-	for rows.Next() {
-		e := db.Historical{}
-		if err = rows.Scan(&e.ID, &e.SymbolUuid, &e.Values, &e.ForDate, &e.CreatedAt); err != nil {
+	var result []documents.Historical
+	for filterCursor.Next(ctx) {
+		history := documents.Historical{}
+		err := filterCursor.Decode(&history)
+		if err != nil {
 			return nil, err
 		}
-		result = append(result, e)
+		result = append(result, history)
 	}
 
 	return &result, nil
