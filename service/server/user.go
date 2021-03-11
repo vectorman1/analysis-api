cf019671519a48df6a1c0120b7c70b8ed6070b67
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-11 20:31:19 +0200

    add handle error method for status responses

--- service/server/user.go
+++ service/server/user.go
@@ -3,7 +3,7 @@ package server
 import (
 	"context"
 
-	"google.golang.org/grpc/status"
+	"github.com/vectorman1/analysis/analysis-api/common"
 
 	"github.com/vectorman1/analysis/analysis-api/generated/user_service"
 	"github.com/vectorman1/analysis/analysis-api/service"
@@ -21,11 +21,7 @@ func NewUserServiceServer(userService *service.UserService) *UserServiceServer {
 func (s *UserServiceServer) Login(ctx context.Context, req *user_service.LoginRequest) (*user_service.LoginResponse, error) {
 	result, err := s.userService.Login(req)
 	if err != nil {
-		st, ok := status.FromError(err)
-		if ok {
-			return nil, st.Err()
-		}
-		return nil, err
+		return nil, common.GetErrorStatus(err)
 	}
 
 	return result, nil
@@ -33,7 +29,7 @@ func (s *UserServiceServer) Login(ctx context.Context, req *user_service.LoginRe
 func (s *UserServiceServer) Register(ctx context.Context, req *user_service.RegisterRequest) (*user_service.RegisterResponse, error) {
 	result, err := s.userService.Register(req)
 	if err != nil {
-		return nil, err
+		return nil, common.GetErrorStatus(err)
 	}
 
 	return result, nil
