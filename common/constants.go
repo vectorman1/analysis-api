368a1caa7fb9b64345eb040ca6f0d7e6826cc307
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-11 20:29:37 +0200

    add mongo constants

--- common/constants.go
+++ common/constants.go
@@ -6,6 +6,10 @@ const TRADING212_INSTRUMENTS_LINK = `https://www.trading212.com/en/Trade-Equitie
 const TRADING212_SHOW_ALL_BUTTON_SELECTOR = `div.conditions-table > div > div.view-more > a`
 const TRADING212_ALL_INSTRUMENTS_SELECTOR = `#all-equities`
 const SYMBOLS_NAMCESPACE = `53edcce7-94d4-4deb-b2ac-d1f6d8657d8e`
+const MONGO_DB_DATABASE = `analysis`
+const OVERVIEWS_COLLECTION = `overviews`
+const HISTORIES_COLLECTION = `histories`
+const TECHNICALS_COLLECTION = `technicals`
 const MaxConcurrency = 2000
 
 func GetAllowedHeaders() []string {
