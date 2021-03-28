fdd742a14a6838d63da8e1b6b5e2fa13fa5b1911
Boris Lachev <boris.lachevbg00@gmail.com>
2021-03-29 01:39:51 +0300

    add random string util method

--- common/utils.go
+++ common/utils.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"math/rand"
 
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/status"
@@ -43,4 +44,22 @@ func GetErrorStatus(err error) error {
 	}
 
 	return err
+}
+
+func RandomStringWithLength(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
